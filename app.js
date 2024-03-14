0e846cc15fee23640650024e66a5f89d85d265d1
juhamikaelele <160930652+Juhamikaelele@users.noreply.github.com>
2024-03-15 00:07:50 +0200

    Simulate fail

--- app.js
+++ app.js
@@ -8,6 +8,13 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static("dist"))
 
+// eslint-disable-next-line no-unused-vars
+app.get("/health", (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw("error...  ")
+  // res.send("ok")
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
